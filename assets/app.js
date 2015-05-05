1a6fb16bebede06bba875a8f4520564d726e1603
David Ash <green.neon@gmail.com>
2015-05-04 19:22:38 -0600

    better data loggin for assume/start

--- assets/app.js
+++ assets/app.js
@@ -1518,11 +1518,13 @@
 		};
 
 		$scope.assumeOrder = function(customerId) {
-			console.log('one-time-use username: '+customerId+' one-time use password: 8847fhhfw485fwkebfwerfv7w458gvwervbkwer8fw5fberubckfckcaer4cbwvb72arkbfrcb1n4hg7');
+			console.log('one-time-use username: '+customerId);
+			console.log('one-time use password: 8847fhhfw485fwkebfwerfv7w458gvwervbkwer8fw5fberubckfckcaer4cbwvb72arkbfrcb1n4hg7');
 		};
 
 		$scope.startOrder = function(customerId) {
-			console.log('one-time-use username: '+customerId+' one-time use password: 8847fhhfw485fwkebfwerfv7w458gvwervbkwer8fw5fberubckfckcaer4cbwvb72arkbfrcb1n4hg7');
+			console.log('one-time-use username: '+customerId);
+			console.log('one-time use password: 8847fhhfw485fwkebfwerfv7w458gvwervbkwer8fw5fberubckfckcaer4cbwvb72arkbfrcb1n4hg7');
 		};
 
 		$scope.specialCharge = customerMgmt.specialCharge;
